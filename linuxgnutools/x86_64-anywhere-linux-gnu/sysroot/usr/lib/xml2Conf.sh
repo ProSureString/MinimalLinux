4424a648ce525d95948ffa2c599b9a69b61db2fb
ProSureString <the.samuel.rus@icloud.com>
2024-05-07 21:48:34 -0700

    redid it(hopefully works)

---
+++ linuxgnutools/x86_64-anywhere-linux-gnu/sysroot/usr/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/opt/build-anywhere/x86_64-anywhere-linux-gnu/x86_64-anywhere-linux-gnu/sysroot/usr/lib"
+XML2_LIBS="-lxml2 -lz  -lpthread   -lm "
+XML2_INCLUDEDIR="-I/opt/build-anywhere/x86_64-anywhere-linux-gnu/x86_64-anywhere-linux-gnu/sysroot/usr/include/libxml2"
+MODULE_VERSION="xml2-2.9.7"
+
---
+++ scripts/anywhere-autoprefix-setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+if [[ "x$BASH_SOURCE" = x"" ]]
+        then BASH_SOURCE=$0
+fi
+
+SCRIPT=$(readlink -f "$BASH_SOURCE")
+SCRIPTPATH=$(dirname "$SCRIPT")
+INSTALLPATH=$(dirname "$SCRIPTPATH")
+
+SYSROOT="${INSTALLPATH}/x86_64-anywhere-linux-gnu/sysroot"
+PREFIX="${SYSROOT}/usr"
+
+NEW_PATH="${PREFIX}/bin:${SYSROOT}/sbin"
+case ":${PATH:=$NEW_PATH}:" in
+    *:${NEW_PATH}:*)  ;;
+    *) PATH="${NEW_PATH}:${PATH}"  ;;
+esac
+
+export PATH="${PATH}"
+export CC="clang --gcc-toolchain=${INSTALLPATH} --sysroot=${SYSROOT}"
+export CXX="clang++ --gcc-toolchain=${INSTALLPATH} --sysroot=${SYSROOT}"
+export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
+export LIBRARY_PATH="${PREFIX}/lib"
+export LDFLAGS="-static-libgcc -static-libstdc++ $1 -fuse-ld=lld -Wl,-z,relro,-z,now -pie -l:libc++.a -l:libc++abi.a -l:libunwind.a -lpthread -ldl -lrt -lz -lm"
+export CPPFLAGS="--gcc-toolchain=${INSTALLPATH} --sysroot=${SYSROOT}"
+export CFLAGS="${CPPFLAGS} -march=x86-64 -fPIC -Oz $2"
+export CXXFLAGS="${CFLAGS} -stdlib=libc++"
+
+export PREFIX="${PREFIX}"
+export CONFIG_SITE="${INSTALLPATH}/config.site"
+echo "prefix=${PREFIX}" > "{$INSTALLPATH}/config.site"
